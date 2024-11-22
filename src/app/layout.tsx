b5c0f36e7bed7666f759d01fb8b9187467ffad09
Yash8077 <ymishra502@gmail.com>
2024-11-22 11:44:58 +0530

    refactor: Update import paths for Header and Footer components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import localFont from "next/font/local";
 import "./globals.css";
-import Header from '@/components/Header'
-import Footer from '@/components/Footer'
+import Header from '@/components/header'
+import Footer from '@/components/footer'
 import { ScrollToTop } from "../components/ScrollToTop";
 import { ScrollProgressBar } from '@/components/ScrollProgressBar'
 import { WishlistProvider } from "@/context/store";
